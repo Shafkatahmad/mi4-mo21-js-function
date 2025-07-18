8124d09fcf0347002d7afee305a53814194bafa1
Shafkatahmad <shafkataahmad@gmail.com>
2025-07-18 22:29:29 +0600

    function declare and call

---
+++ function.js
@@ -0,0 +1,25 @@
+const price = 45;
+
+// function declare
+function fanOffKor() {
+  console.log('bosa theke uthe dara');
+  console.log('Walk towards the switch');
+  console.log('Click the switch off of the fan');
+}
+
+// call the fucntion
+fanOffKor();
+
+
+
+// function name() {
+// 
+// }
+function brushTeeth() {
+  console.log('pick up the brush');
+  console.log('add paste');
+  console.log('brush for 2 minutes');
+}
+
+// call the function
+brushTeeth();
